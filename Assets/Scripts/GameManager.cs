d727bf7429f66b69e5f01ab3a58b758bd1f522ab
Bene_Dev <seriousbenedikt@web.de>
2018-05-19 16:17:58 +0200

    Shooting Object pooling fixed and shoots in right direction of turret rotation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -32,13 +32,9 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    private void Update()
-    {
-        print(cannonBallParent.transform.childCount);
-    }
-
     public GameObject GetCannonBall(Vector3 positionToSpawn, Vector3 direction)
     {
+        // TODO make direction aim a little bit higher for nicer flying curve
         GameObject ballToReturn = freeCannonBalls.Pop();
         ballToReturn.transform.position = positionToSpawn;
         ballToReturn.transform.forward = direction;
