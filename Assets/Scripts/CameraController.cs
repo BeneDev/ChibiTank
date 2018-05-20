179a97ad55229572f911bec20da37b17df1a4e1d
Bene_Dev <seriousbenedikt@web.de>
2018-05-20 10:07:37 +0200

    Camera follows player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    GameObject player;
+
+    [SerializeField] float speed = 5f;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.transform.position, speed);
+    }
+}
