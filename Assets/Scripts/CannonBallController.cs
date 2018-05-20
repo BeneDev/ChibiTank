398022867905b2bb96d5d86752abb41757a17343
Bene_Dev <seriousbenedikt@web.de>
2018-05-20 09:31:03 +0200

    Cannonball plays 3D sound when hitting something, depending on the surface

--- Assets/Scripts/CannonBallController.cs
+++ Assets/Scripts/CannonBallController.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class CannonBallController : MonoBehaviour {
 
     Rigidbody rb;
+    AudioSource audioSource;
+
     [SerializeField] float speed = 1f;
     [SerializeField] float durationUntilDespawn = 3f;
     float timeWhenFired = 0f;
 
+    [SerializeField] AudioClip[] impactSounds;
+
+    Dictionary<string, int> audioDict = new Dictionary<string, int>();
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        audioDict["terrain"] = 0;
+        audioDict["mechanical"] = 1;
+        audioDict["organic"] = 2;
     }
 
     private void OnEnable()
@@ -28,6 +38,28 @@ public class CannonBallController : MonoBehaviour {
             gameObject.SetActive(false);
             gameObject.transform.SetParent(GameManager.Instance.cannonBallParent.transform);
             GameManager.Instance.freeCannonBalls.Push(gameObject);
+        }
+    }
+
+    void PlaySound(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Terrain"))
+        {
+            PlaySound(impactSounds[audioDict["terrain"]]);
+        }
+        else if(collision.gameObject.layer == LayerMask.NameToLayer("Mechanical"))
+        {
+            PlaySound(impactSounds[audioDict["mechanical"]]);
+        }
+        else if (collision.gameObject.layer == LayerMask.NameToLayer("Organic"))
+        {
+            PlaySound(impactSounds[audioDict["organic"]]);
         }
     }
 }
