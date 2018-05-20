c1af072469bae3940fb408bf6e07fa2c953ff8ec
Bene_Dev <seriousbenedikt@web.de>
2018-05-20 20:01:19 +0200

    Low Poly Water prefab

---
+++ Assets/Scripts/WaterNoiseGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterNoiseGenerator : MonoBehaviour {
+
+    [SerializeField] float power = 3f;
+    [SerializeField] float scale = 1f;
+    [SerializeField] float timeScale = 1f;
+
+    float offsetX;
+    float offsetY;
+    MeshFilter meshFilter;
+
+	// Use this for initialization
+	void Start () {
+        meshFilter = GetComponent<MeshFilter>();
+        MakeNoise();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MakeNoise();
+        offsetX += Time.deltaTime * timeScale;
+        offsetY += Time.deltaTime * timeScale;
+	}
+
+    void MakeNoise()
+    {
+        Vector3[] verts = meshFilter.mesh.vertices;
+        for (int i = 0; i < verts.Length; i++)
+        {
+            verts[i].y = CalculateHeight(verts[i].x, verts[i].z) * power;
+        }
+
+        meshFilter.mesh.vertices = verts;
+    }
+
+    float CalculateHeight(float x, float y)
+    {
+        float xCord = x * scale + offsetX;
+        float yCord = y * scale + offsetY;
+
+        return Mathf.PerlinNoise(xCord, yCord);
+    }
+}
---
+++ Assets/Scripts/WaterPlaneGenerator.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPlaneGenerator : MonoBehaviour {
+
+    [SerializeField] float planeSize = 1f;
+    [SerializeField] int planeGridSize = 16;
+
+    private MeshFilter meshFilter;
+
+	// Use this for initialization
+	void Start () {
+        meshFilter = GetComponent<MeshFilter>();
+        meshFilter.mesh = GenerateMesh();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private Mesh GenerateMesh()
+    {
+        Mesh m = new Mesh();
+        List<Vector3> verticies = new List<Vector3>();
+        List<Vector3> normals = new List<Vector3>();
+        List<Vector2> uvs = new List<Vector2>();
+
+        for (int x = 0; x < planeGridSize + 1; x++)
+        {
+            for (int y = 0; y < planeGridSize + 1; y++)
+            {
+                verticies.Add(new Vector3(-planeSize * 0.5f + planeSize * (x / (float)planeGridSize), 0, -planeSize * 0.5f + planeSize * (y / (float)planeGridSize)));
+                normals.Add(Vector3.up);
+                uvs.Add(new Vector2(x / (float)planeGridSize, y / (float)planeGridSize));
+            }
+        }
+
+        List<int> tris = new List<int>();
+        int vertCount = planeGridSize + 1;
+        for (int i = 0; i < vertCount * vertCount - vertCount; i++)
+        {
+            if((i + 1) % vertCount == 0)
+            {
+                continue;
+            }
+            tris.AddRange
+            (
+                new List<int>()
+                {
+                    i + 1 + vertCount,
+                    i + vertCount,
+                    i,
+                    i,
+                    i + 1,
+                    i + 1 + vertCount
+                }
+            );
+        }
+
+        m.SetVertices(verticies);
+        m.SetNormals(normals);
+        m.SetUVs(0, uvs);
+        m.SetTriangles(tris, 0);
+
+        return m;
+    }
+}
