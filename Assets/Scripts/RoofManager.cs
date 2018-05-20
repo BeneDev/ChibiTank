626c4c3f93566909155c8531bbc3e45688c49807
Bene_Dev <seriousbenedikt@web.de>
2018-05-20 13:15:03 +0200

    Own Fade animation
    Not working

--- Assets/Scripts/RoofManager.cs
+++ Assets/Scripts/RoofManager.cs
@@ -6,19 +6,35 @@ public class RoofManager : MonoBehaviour {
 
     [SerializeField] GameObject roof;
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if(roof)
+        if(roof && other.tag == "Player")
         {
+            StartCoroutine(FadeAlphaTo(0f, 1f));
             roof.SetActive(false);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if(roof)
+        if (roof && other.tag == "Player")
         {
+            //StartCoroutine(FadeAlphaTo(1f, 1f));
             roof.SetActive(true);
         }
+    }
+
+    IEnumerator FadeAlphaTo(float value, float duration)
+    {
+        Color color = roof.GetComponent<MeshRenderer>().material.color;
+        float initalAlpha = color.a;
+        for (float t = 0; t < duration; t += Time.deltaTime)
+        {
+            color.a = (value - initalAlpha) / duration;
+            roof.GetComponent<MeshRenderer>().material.color = color;
+            yield return new WaitForEndOfFrame();
+        }
+        color.a = value;
+        roof.GetComponent<MeshRenderer>().material.color = color;
     }
 }
