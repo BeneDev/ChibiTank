940c0ede027462709013fb72f992cc509021fab1
Bene_Dev <seriousbenedikt@web.de>
2018-05-21 18:41:28 +0200

    Pre Button Animation

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -5,6 +5,39 @@ using UnityEngine.UI;
 
 public class PauseMenu : StandardMenu {
 
-    
+    public void OnOptionsButtonClicked()
+    {
+        
+    }
+
+    public void OnMapButtonClicked()
+    {
+
+    }
+
+    public void OnQuestsButtonClicked()
+    {
+
+    }
+
+    public void OnStatusButtonClicked()
+    {
+
+    }
+
+    public void OnLoadoutButtonClicked()
+    {
+
+    }
+
+    public void OnSaveButtonClicked()
+    {
+
+    }
+
+    public void OnQuitButtonClicked()
+    {
+
+    }
 
 }
