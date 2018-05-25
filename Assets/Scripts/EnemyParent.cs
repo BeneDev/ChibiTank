797eb544470de7214e8aba5db4dbb563f2140561
Bene_Dev <seriousbenedikt@web.de>
2018-05-25 19:16:17 +0200

    Parent for enemy classes

---
+++ Assets/Scripts/EnemyParent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is the script, all tank enemies inherit from
+/// </summary>
+public class EnemyParent : MonoBehaviour {
+
+    
+
+}
