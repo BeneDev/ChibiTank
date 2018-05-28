0776e5b2451a2fb56dac6711083fee8dc871b1e2
Bene_Dev <seriousbenedikt@web.de>
2018-05-28 20:12:01 +0200

    Player can zoom in and out on map with scroll wheel

---
+++ Assets/Scripts/MapNavigationController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MapNavigationController : MonoBehaviour {
+
+    Camera mapCam;
+    [SerializeField] float zoomSpeed = 5f;
+    [SerializeField] float minZoom = 5f;
+    [SerializeField] float maxZoom = 500f;
+
+    private void Awake()
+    {
+        mapCam = GameObject.FindGameObjectWithTag("MapCam").GetComponent<Camera>() as Camera;
+        SetZoom(maxZoom);
+    }
+
+    private void Update()
+    {
+        mapCam.orthographicSize = Mathf.Clamp(mapCam.orthographicSize - (Input.GetAxis("Mouse ScrollWheel") * zoomSpeed != 0 ? Mathf.Sign(Input.GetAxis("Mouse ScrollWheel")) * zoomSpeed : 0), minZoom, maxZoom);
+    }
+    
+    void SetZoom(float zoomLevel)
+    {
+        mapCam.orthographicSize = zoomLevel;
+    }
+}
