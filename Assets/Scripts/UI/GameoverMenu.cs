92dde1ae06015e53c2db29214ac08aa842571d4c
BeneDev <seriousbenedikt@web.de>
2018-05-29 10:25:47 +0200

    Load menu created

---
+++ Assets/Scripts/UI/GameoverMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameoverMenu : Menu<GameoverMenu> {
+
+    public static void Show()
+    {
+        Open();
+    }
+
+    public static void Hide()
+    {
+        Close();
+    }
+
+    public void OnQuitButtonClicked()
+    {
+        Application.Quit();
+    }
+
+    public void OnLoadSaveButtonClicked()
+    {
+        LoadMenu.Show();
+    }
+
+}
---
+++ Assets/Scripts/UI/LoadMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadMenu : Menu<LoadMenu> {
+
+    public static void Show()
+    {
+        Open();
+        // Pause the game
+        Time.timeScale = 0f;
+    }
+
+    public static void Hide()
+    {
+        // Unpause the game
+        Time.timeScale = 1f;
+        Close();
+    }
+
+    public void OnLoadButtonClicked()
+    {
+        //TODO load game somehow
+        GameObject.FindGameObjectWithTag("Player").transform.position = GameManager.Instance.RespawnPoint;
+        Hide();
+    }
+
+    public void OnCancelButtonClicked()
+    {
+        Hide();
+    }
+}
