4bf6bacf94ee9bbf392f1ca9fe1e08b65dc1fe68
Bene_Dev <seriousbenedikt@web.de>
2018-05-29 21:51:01 +0200

    Detection if Mouse is over a certain equipment image

---
+++ Assets/Scripts/UI/MouseOverEquipmentController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseOverEquipmentController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    bool isOver = false;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        print("is over");
+        isOver = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        print("leaves");
+        isOver = false;
+    }
+}
