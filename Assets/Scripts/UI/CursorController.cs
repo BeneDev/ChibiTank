faeb04eb58ea3ab98d5081489d73b584ffb00654
Bene_Dev <seriousbenedikt@web.de>
2018-06-06 19:27:53 +0200

    Different ingame cursor now
    sprite is only the first iteration

--- Assets/Scripts/UI/CursorController.cs
+++ Assets/Scripts/UI/CursorController.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class CursorController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] Texture2D cursorTexture;
+    Vector2 hotSpot;
+
+    private void Awake()
+    {
+        if (cursorTexture)
+        {
+            hotSpot = new Vector2(cursorTexture.width / 2f, cursorTexture.height / 2f);
+            Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.Auto);
+        }
+    }
 }
